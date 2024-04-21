073dbb2d45a880fae1ef5f6cba8fefa8fffbcd09
KangSeungBae <kangseungbae00@gmail.com>
2024-04-21 14:48:32 +0900

    1111

---
+++ 240421_test/src/test2/copy/test2.java
@@ -0,0 +1,10 @@
+package test2.copy;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hellddddo New World");
+
+	}
+
+}
---
+++ 240421_test/src/test3/copy/Hello.java
@@ -0,0 +1,10 @@
+package test3.copy;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hellddddo New World");
+
+	}
+
+}
